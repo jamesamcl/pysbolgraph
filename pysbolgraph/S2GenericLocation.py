0a2be87212e0f0611a6b5e60b9c205c54f9dac82
James Scott-Brown <james@jamesscottbrown.com>
2018-06-22 11:26:10 +0100

    call super() on right object

--- pysbolgraph/S2GenericLocation.py
+++ pysbolgraph/S2GenericLocation.py
@@ -5,8 +5,8 @@ from terms import SBOL2, Dcterms
 
 class S2GenericLocation(S2Location):
     def __init__(self, g, uri):
-        super(S2Range, self).__init__(g, uri)
+        super(S2GenericLocation, self).__init__(g, uri)
     @property
     def orientation(self):
         return self.getUriProperty(SBOL2.orientation)
-    
+
